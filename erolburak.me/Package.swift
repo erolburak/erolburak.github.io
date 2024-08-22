37314a7fd739105e9e6b81443afd8abab910e685
Burak Erol <140210017+erolburak@users.noreply.github.com>
2024-08-23 00:26:36 +0200

    update swift format 6.0 (#62)

--- erolburak.me/Package.swift
+++ erolburak.me/Package.swift
@@ -1,12 +1,38 @@
-// swift-tools-version:5.5
+// swift-tools-version: 6.0
 
 import PackageDescription
 
-let package = Package(name: "ErolburakMe",
-					  platforms: [.macOS(.v12)],
-					  products: [.executable(name: "ErolburakMe",
-											 targets: ["ErolburakMe"])],
-					  dependencies: [.package(url: "https://github.com/Ze0nC/Publish.git", 
-											  branch: "Multi-Language")],
-					  targets: [.executableTarget(name: "ErolburakMe",
-												  dependencies: ["Publish"])])
+let package = Package(
+    name: "ErolburakMe",
+    platforms: [
+        .macOS(
+            .v12
+        )
+    ],
+    products: [
+        .executable(
+            name: "ErolburakMe",
+            targets: [
+                "ErolburakMe"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Ze0nC/Publish.git",
+            branch: "Multi-Language"
+        ),
+        .package(
+            url: "https://github.com/nicklockwood/SwiftFormat.git",
+            branch: "main"
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "ErolburakMe",
+            dependencies: [
+                "Publish"
+            ]
+        )
+    ]
+)
